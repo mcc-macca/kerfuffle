2aae76e749fd3fcc2736442da1eee471bd7daf45
macca <85993132+mcc-macca@users.noreply.github.com>
2022-03-29 10:24:20 -0400

    Add files via upload

---
+++ alpha-mcc.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+# Color variables
+red='\033[0;31m'
+green='\033[0;32m'
+yellow='\033[0;33m'
+blue='\033[0;34m'
+magenta='\033[0;35m'
+cyan='\033[0;36m'
+# Clear the color after that
+clear='\033[0m'
+
+echo "*******************************"
+echo "*                             *"
+echo -e "*     (C) ${red}Macca${clear} ${magenta}Computer${clear}      *"
+echo "*                             *"
+echo -e "*    ${cyan}Copyright${clear}  ${green}2018${clear}-${yellow}2022${clear}     *"
+echo "*                             *"
+echo "*******************************"
+echo "  "
+echo "This source is from https://maccacomputer.altervista.org/product/kerfuffle/ . Original Macca Computer Software"
+echo "   "
+echo -e "WELCOME TO ${red}K${clear}${green}E${clear}${yellow}R${clear}${blue}F${clear}${magenta}U${clear}${cyan}F${clear}${red}F${clear}${green}L${clear}${yellow}E${clear}!"
+echo "  "
+echo "Would you like to start the game? (yes/no)"
+
